b718391fea7c1e5ded38c10536f0d4b03e214c6b
simonklejnstrup <63997120+simonklejnstrup@users.noreply.github.com>
2021-06-22 12:59:42 +0200

    Initial commit

---
+++ src/main/java/kea/eksamen/controller/MyController.java
@@ -0,0 +1,109 @@
+package kea.eksamen.controller;
+
+
+import kea.eksamen.model.database.Kommune;
+import kea.eksamen.model.database.Sogn;
+import kea.eksamen.service.Dataservice;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.context.request.WebRequest;
+
+import java.time.LocalDate;
+
+@Controller
+public class MyController {
+
+    private Dataservice dataService;
+
+    public MyController(Dataservice dataService) {
+        this.dataService = dataService;
+    }
+
+    @GetMapping("/")
+    public String hentSogne(Model model) {
+        model.addAttribute("sogne", dataService.getAllSogne());
+        model.addAttribute("kommuner", dataService.getAllKommuner());
+
+
+        return "index";
+    }
+
+    @PostMapping("/create")
+    public String opretSogn(WebRequest request) {
+
+        int sogneKode = Integer.parseInt(request.getParameter("sogneKode"));
+        String sogneNavn = request.getParameter("sogneNavn");
+        String kommuneNavn = request.getParameter("kommune");
+        int incidens = Integer.parseInt(request.getParameter("incidens"));
+        //LocalDate nedlukning = LocalDate.parse(request.getParameter("nedlukning"));
+
+        dataService.createSogn(sogneKode, kommuneNavn, sogneNavn, incidens);
+
+        return "redirect:/";
+    }
+
+
+
+
+    @DeleteMapping(value = "/delete/{navn}")
+    public String deleteSogn(@PathVariable String navn, Model model) {
+        Sogn sogn = dataService.findByName(navn);
+        sogn.setKommune(null);
+        dataService.updateSogn(sogn.getSogneKode(), sogn.getKommune(), sogn.getNavn(), sogn.getIncidens());
+
+        dataService.deleteSogn(navn);
+
+        model.addAttribute("sogne", dataService.getAllSogne());
+        model.addAttribute("kommuner", dataService.getAllKommuner());
+
+        return "index";
+    }
+
+
+
+
+
+    @PostMapping("/close")
+    public String closeSogn(WebRequest request, Model model) {
+        String sogneNavn = request.getParameter("sogneNavn");
+
+        dataService.closeSogn(sogneNavn);
+
+        model.addAttribute("sogne", dataService.getAllSogne());
+        model.addAttribute("kommuner", dataService.getAllKommuner());
+
+
+        return "index";
+    }
+
+    @GetMapping("/update/{navn}")
+    public String updateSogn(@PathVariable("navn") String navn, Model model){
+        //System.out.println(dataService.findByName(navn).getSogneKode());
+
+        model.addAttribute("sogn", dataService.findByName(navn));
+
+        return "update";
+    }
+
+
+    @PostMapping("/update")
+    public String update(WebRequest request, Model model) {
+        String sogneNavn = request.getParameter("navn");
+        int incidens = Integer.parseInt(request.getParameter("incidens"));
+        Kommune kommune = new Kommune(request.getParameter("kommune"));
+        int sogneKode = Integer.parseInt(request.getParameter("sogneKode"));
+
+
+        dataService.updateSogn(sogneKode,kommune, sogneNavn, incidens);
+
+        model.addAttribute("sogne", dataService.getAllSogne());
+        model.addAttribute("kommuner", dataService.getAllKommuner());
+
+        return "index";
+        //return "redirect:/";
+    }
+
+
+
+}
